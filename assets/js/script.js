4611fe838649655ea598e9df2fd164fd03a8f2fd
akaur091 <amniderkaur.09@gmail.com>
2022-04-01 14:28:43 -0700

    pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// what needs to be done?
+// button to start game
+    // button element in html
+// create a timer
+    // timer starts when button is pressed (add event listner connected to the start button)
+    // ends game if word is not guessed
+    // timer resets when start button is clicked again
+// create a variable of words- array of words in game
+    // change from actual letter to blank underline and vice versa
+    // loop that takes word into the blank spaces in game
+    // iterate through the word and place correct letter in correct spot
+    // if wrong, then no return
+    // game done when either timer runs out or the word is guessed correctly
+// wins and losses
+    //  intially set to zero
+    // increment based on results
+    // store those values on local storage
+    // display message (alert)
+// use key event
